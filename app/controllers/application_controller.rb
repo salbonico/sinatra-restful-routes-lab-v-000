29715b886a1dab91d8c48828ce53377294e0e886
Steve Albonico <salbonico@gmail.com>
2019-02-10 18:00:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ end
 post '/recipes' do
   @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
 
-  redirect "/recipes"
+  redirect "/recipes/#{@recipe.id}"
 end
 
 get '/recipes/:id' do
